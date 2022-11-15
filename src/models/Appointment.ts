1be8c49f21424e8c89e5a7edd6738bf3a73919e3
Luiz Verissimo <luizverissimosouza@gmail.com>
2022-11-15 01:16:31 -0300

    creating foreign key of appointments and users

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -4,15 +4,22 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
 
+import User from './User';
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
