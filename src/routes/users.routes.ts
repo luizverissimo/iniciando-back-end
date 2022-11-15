c4529390962530084ec617e16d6bbfd71dfe8054
Luiz Verissimo <luizverissimosouza@gmail.com>
2022-11-15 18:20:42 -0300

    ading create users

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import CreateUserService from '../service/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+    return response.json(user);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+});
+
+export default usersRouter;
